9cede74f6185850c60fd9d2c0fd395496d388f7d
moluoxixi <1983531544@qq.com>
2025-05-12 14:15:47 +0800

    feat: 修改CICD脚本

--- scripts/CICD.mjs
+++ scripts/CICD.mjs
@@ -15,9 +15,18 @@ const appCode = envObj['VITE_GLOB_APP_CODE'].replace(/["']/g, '');
 
 const ciPath = path.resolve(__dirname, '../.gitlab-ci.yml');
 const ciText = fs.readFileSync(ciPath, 'utf-8');
-const newCiText = ciText.replace(/variables:\s+systemCode: '.*'/, `variables:\r  systemCode: '${appCode}'`);
-
-fs.writeFileSync(ciPath, newCiText);
-
-execSync('git add .gitlab-ci.yml');
-execSync('git commit -m "chore: update appCode in .gitlab-ci.yml"');
+const ciLines = ciText.split(/\r?\n/);
+const index = ciLines.findIndex(line => line.startsWith('variables:'));
+if (index === -1) {
+  throw new Error('not found `variables:` in .gitlab-ci.yml');
+}
+const newValue = `variables:\r  systemCode: '${appCode}'`;
+if (ciLines[index] !== newValue) {
+  ciLines[index] = newValue;
+  const newCiText = ciLines.join('\r\n');
+  fs.writeFileSync(ciPath, newCiText);
+  execSync('git add .gitlab-ci.yml');
+  execSync('git commit -m "chore: update appCode in .gitlab-ci.yml"');
+}else{
+  console.log('no need update');
+}
