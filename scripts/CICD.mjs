dd2a81f9c6c0d71d16cd30f55c40d3243c27203c
moluoxixi <1983531544@qq.com>
2025-05-12 14:19:29 +0800

    feat: cs

--- scripts/CICD.mjs
+++ scripts/CICD.mjs
@@ -15,18 +15,16 @@ const appCode = envObj['VITE_GLOB_APP_CODE'].replace(/["']/g, '');
 
 const ciPath = path.resolve(__dirname, '../.gitlab-ci.yml');
 const ciText = fs.readFileSync(ciPath, 'utf-8');
-const ciLines = ciText.split(/\r?\n/);
-const index = ciLines.findIndex(line => line.startsWith('variables:'));
-if (index === -1) {
-  throw new Error('not found `variables:` in .gitlab-ci.yml');
-}
-const newValue = `variables:\r  systemCode: '${appCode}'`;
-if (ciLines[index] !== newValue) {
-  ciLines[index] = newValue;
-  const newCiText = ciLines.join('\r\n');
+const oldCiText = ciText.match(/variables:\s+systemCode: '(.*)'/);
+const oldAppCode = oldCiText && oldCiText[1];
+
+if (oldAppCode === appCode) {
+  console.log('appCode in .gitlab-ci.yml is the same as the environment variable, no need to update');
+} else {
+  const newCiText = ciText.replace(/variables:\s+systemCode: '.*'/, `variables:\r  systemCode: '${appCode}'`);
+
   fs.writeFileSync(ciPath, newCiText);
+
   execSync('git add .gitlab-ci.yml');
   execSync('git commit -m "chore: update appCode in .gitlab-ci.yml"');
-}else{
-  console.log('no need update');
 }
