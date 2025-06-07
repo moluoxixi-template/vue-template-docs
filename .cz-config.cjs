02e61cd35870328c8cb3301f173d28b8037490fb
moluoxixi <1983531544@qq.com>
2025-06-07 13:31:17 +0800

    feat: 测试新脚本

--- .cz-config.cjs
+++ .cz-config.cjs
@@ -1,54 +1,29 @@
 module.exports = {
-  types: {
-    feat: {
-      description: '新增功能，迭代项目需求',
-      title: 'Features',
+  types: [
+    { value: 'fix', name: 'fix:      修复缺陷' },
+    { value: 'feat', name: 'feat:     新增功能，迭代项目需求' },
+    {
+      value: 'style',
+      name: 'style:    代码格式修改, 注意不是 css 修改',
     },
-    fix: {
-      description: '修复缺陷',
-      title: 'Bug Fixes',
+    {
+      value: 'refactor',
+      name: 'refactor: 重构代码，非新增功能也非修复缺陷',
     },
-    refactor: {
-      description: '重构代码，非新增功能也非修复缺陷',
-      title: 'Code Refactoring',
+    {
+      value: 'perf',
+      name: 'perf:     优化相关，比如提升性能、体验',
     },
-    perf: {
-      description: '优化相关，比如提升性能、体验',
-      title: 'Performance Improvements',
+    {
+      value: 'chore',
+      name: 'chore:    其他修改, 比如改变构建流程、或者增加依赖库、工具等',
     },
-    test: {
-      description: '新增测试，追加测试用例验证代码',
-      title: 'Tests',
-    },
-    style: {
-      description: '代码格式修改, 注意不是 css 修改',
-      title: 'Styles',
-    },
-    revert: {
-      description: '回滚版本，撤销某次代码提交',
-      title: 'Reverts',
-    },
-    merge: {
-      description: '合并分支，合并分支代码到其他分支',
-      title: 'Merges',
-    },
-    docs: {
-      description: '更新文档，仅修改文档不修改代码',
-      title: 'Documentation',
-    },
-    build: {
-      description: '编译相关的修改，例如发布版本、对项目构建或者依赖的改动',
-      title: 'Builds',
-    },
-    chore: {
-      description: '其他修改, 比如改变构建流程、或者增加依赖库、工具等',
-      title: 'Chores',
-    },
-    ci: {
-      description: '更新脚本，改动CI或执行脚本配置',
-      title: 'Continuous Integrations',
-    },
-  },
+    { value: 'revert', name: 'revert:   回滚版本，撤销某次代码提交' },
+    { value: 'docs', name: 'docs:    更新文档，仅修改文档不修改代码' },
+    { value: 'test', name: 'test:     新增测试，追加测试用例验证代码' },
+    { value: 'ci', name: 'ci:      更新脚本，改动CI或执行脚本配置' },
+  ],
+  // 自定义消息提示
   messages: {
     type: '选择你要提交的更改类型:',
     scope: '此更改的范围是什么(例如组件或文件名)(可选):',
@@ -59,25 +34,42 @@ module.exports = {
     footer: '列出此更改关闭的issues(可选按回车跳过):',
     confirmCommit: '确认提交上述内容?',
   },
-  usePreparedCommit: false,
-  allowTicketNumber: false,
-  isTicketNumberRequired: false,
-  ticketNumberPrefix: 'TICKET-',
-  ticketNumberRegExp: '\\d{1,5}',
-  allowCustomScopes: false,
-  allowBreakingChanges: ['feat', 'fix'],
+  // 限制主题长度
   subjectLimit: 100,
-  scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+  // // 跳过你想要的问题
+  skipQuestions: ['scope','customScope', 'body','breaking','footer'],
+  // // 定义可选范围
+  // scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+  //
+  // // 是否使用预备提交信息
+  // usePreparedCommit: false, // 从./.git/COMMIT_EDITMSG中重用提交信息
+  // // 是否允许票号
+  // allowTicketNumber: false,
+  // // 是否要求票号
+  // isTicketNumberRequired: false,
+  // // 票号前缀
+  // ticketNumberPrefix: 'TICKET-',
+  // // 票号正则表达式
+  // ticketNumberRegExp: '\\d{1,5}',
+  //
+  // // 范围覆盖，需要与类型字段的值匹配。例如：'fix'
   // scopeOverrides: {
   //   fix: [
-  //     { name: 'merge' },
-  //     { name: 'style' },
-  //     { name: 'e2eTest' },
-  //     { name: 'unitTest' }
+  //     {name: 'merge'},
+  //     {name: 'style'},
+  //     {name: 'e2eTest'},
+  //     {name: 'unitTest'}
   //   ]
   // },
-  // skipQuestions: ['scope', 'body'],
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true, // default is false
-}
+  // // 是否允许自定义范围
+  // allowCustomScopes: true,
+  // // 允许破坏性更改的类型
+  // allowBreakingChanges: ['feat', 'fix'],
+
+  // // 换行符，用于body和footer字段
+  // breaklineChar: '|',
+  // // 页脚前缀
+  // footerPrefix : 'ISSUES CLOSED:',
+  // // 是否首先询问破坏性更改，默认是false
+  // askForBreakingChangeFirst : true,
+};
