4037fc73b05d519204c2cde3e0db0a3709fb3c25
moluoxixi <1983531544@qq.com>
2025-06-24 17:29:52 +0800

    fix: 修复插件问题

--- plugins/addScopedAndReplacePrefix.ts
+++ plugins/addScopedAndReplacePrefix.ts
@@ -27,6 +27,7 @@ export default function addScopedAndReplacePrefixPlugin({
     configResolved(config: any) {
       isProduction = config.command === 'build' || config.isProduction
     },
+    enforce: 'post',
     transform(code = '', id = '') {
       if (!isProduction && !useDevMode) return code
       if (!oldPrefix || !newPrefix) return code
