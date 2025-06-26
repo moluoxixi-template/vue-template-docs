e95dad49b404755258fa5f3f08a4bfaeb1da6364
moluoxixi <1983531544@qq.com>
2025-06-26 19:42:49 +0800

    docs: 更新 GitHub 评论配置

--- .docs/.vitepress/constants/index.ts
+++ .docs/.vitepress/constants/index.ts
@@ -11,4 +11,14 @@ export const githubConfig = {
    * 管理员账号
    */
   admin: 'moluoxixi',
+  /**
+   * https://github.com/settings/developers
+   * github授权码 ID
+   */
+  clientID: 'Ov23lib6kV0uel05wgGI',
+  /**
+   * https://github.com/settings/developers
+   * github授权码 Secret
+   */
+  clientSecret: '91c64584a4f0738475e93d02c3ab210e74258a6f',
 }
--- .docs/.vitepress/theme/components/layout/components/Comments.vue
+++ .docs/.vitepress/theme/components/layout/components/Comments.vue
@@ -23,11 +23,11 @@ onContentUpdated(() => {
     return
   }
   deleteChild(element)
-  const { repo, owner, admin } = githubConfig
+  const { repo, owner, admin, clientID, clientSecret } = githubConfig
   // 需要去这个网站注册 https://github.com/settings/developers
   const gitalk = new Gitalk({
-    clientID: 'Ov23liGIwl5Jf43MRtWf',
-    clientSecret: 'd5272ed884d1ed79e502c27ae225d8d9e9fc184c',
+    clientID,
+    clientSecret,
     // 仓库名
     repo,
     // 所属用户或组织名
