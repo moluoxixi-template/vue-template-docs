c829bae600ca0f8e025a8706c4c3d2ce61c2b7ce
moluoxixi <1983531544@qq.com>
2025-07-15 17:07:55 +0800

    feat: 修改viteConfig和eslintConfig为依赖

--- src/components/ViteConfig/src/plugins/autoRoutes/index.ts
+++ src/components/ViteConfig/src/plugins/autoRoutes/index.ts
@@ -3,7 +3,7 @@ import { normalizePath } from 'vite'
 import path from 'node:path'
 import fs from 'node:fs'
 // autoRoutes/index.ts
-import { findDefaultRoute, findParentRoute } from './routeGenerator.ts'
+import { findDefaultRoute, findParentRoute, generateRoutes } from './routeGenerator.ts'
 
 interface RouteModule {
   path: string
@@ -115,9 +115,10 @@ function createAutoRoutesPlugin({ routeConfig, virtualModuleId, dts, root }: con
         // 生成路由JS代码
         const code = `
           ${imports.join('\n')}
-          const routes = [${routes.join(',\n')}];
+          const generateRoutes = ${generateRoutes};
           const findDefaultRoute = ${findDefaultRoute};
           const findParentRoute = ${findParentRoute};
+          const routes = [${routes.join(',\n')}];
           export { routes, findDefaultRoute };
           export default routes;
         `
