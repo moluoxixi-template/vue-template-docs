1297b9f6a3c923d354cb9fd9beaf960574a03b86
moluoxixi <1983531544@qq.com>
2025-07-18 15:03:01 +0800

    feat: 优化导出

--- src/components/ExportExcel/index.ts
+++ src/components/ExportExcel/index.ts
@@ -1,8 +1,10 @@
 import ExportExcel from './src/index.vue'
 import type { App } from 'vue'
+import Example from './src/Example.vue'
 
 // 导出组件
 export default ExportExcel
+export { Example }
 
 // 用于Vue插件形式注册
 export function install(app: App) {
