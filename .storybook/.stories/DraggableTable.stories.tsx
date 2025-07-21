053762a47f8e896ea368191d2115175fe77d15ef
moluoxixi <1983531544@qq.com>
2025-07-21 12:52:56 +0800

    fix: 修复tsconfig错误

--- .storybook/.stories/DraggableTable.stories.tsx
+++ .storybook/.stories/DraggableTable.stories.tsx
@@ -3,7 +3,7 @@
 import type { Meta, StoryFn, StoryObj } from '@storybook/vue3'
 import DraggableTable from '@/components/DraggableTable/index.ts'
 import { ref } from 'vue'
-import type { TableRowData } from '@/components/DraggableTable/_types'
+import type { TableRowData } from '@/components/DraggableTable/src/_types'
 
 /**
  * 可拖拽表格组件
