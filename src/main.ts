19372485ec29f2f8dca9287212090d2aacd56cec
moluoxixi <1983531544@qq.com>
2025-07-28 10:32:51 +0800

    feat: 提供Split组件

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,9 @@ import { useSystemStore } from './stores/modules/system.ts'
 
 import '@/assets/styles/main.css'
 
+import '@/assets/styles/element/index.scss'
+import 'vxe-table/lib/style.css'
+
 import '@/assets/fonts/index.css'
 
 import 'moment/dist/locale/zh-cn' // 中文化
